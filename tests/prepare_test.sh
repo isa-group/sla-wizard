963f2e7e60b60ae24b83ad8b16af74d3db059e2f
Ignacio Peluaga <ipeluaga@gmail.com>
2023-09-27 23:58:14 +0200

    Changed name of cloned folder

--- tests/prepare_test.sh
+++ tests/prepare_test.sh
@@ -2,29 +2,29 @@ PROXY=$1
 
 set -xe
 
-git clone https://github.com/isa-group/sla-gateway-benchmark /tmp/autocloned-sla-gateway-benchmark
+git clone https://github.com/isa-group/sla-gateway-benchmark /tmp/sla-gateway-benchmark
 
 mkdir -p /tmp/generatedSLAs
 
 SLAS_TO_PRODUCE=$2 \
 APIKEYS_PER_SLA=$3 \
 GENERATED_LOCATION=/tmp/generatedSLAs \
-node /tmp/autocloned-sla-gateway-benchmark/autoSLAs/index.js
+node /tmp/sla-gateway-benchmark/autoSLAs/index.js
 
 echo "# Creating proxy configuration file with sla-wizard for $PROXY" ;
 node src/index.js config --authLocation header $PROXY \
-			--oas /tmp/autocloned-sla-gateway-benchmark/specs/simple_api_oas.yaml \
+			--oas /tmp/sla-gateway-benchmark/specs/simple_api_oas.yaml \
 			--sla /tmp/generatedSLAs/ \
 			--outFile /tmp/proxy-configuration-file ;
 		
 echo "# Starting containerized test bed based on Docker-Compose" ;
 if [ "$PROXY" = "traefik" ]; then
-    D_CFG_PATH=/tmp/proxy-configuration-file CFG_PATH=/tmp/autocloned-sla-gateway-benchmark/proxies/traefik/traefik.yaml docker-compose \
-        --file /tmp/autocloned-sla-gateway-benchmark/proxies/traefik/docker-compose-traefik.yaml up \
+    D_CFG_PATH=/tmp/proxy-configuration-file CFG_PATH=/tmp/sla-gateway-benchmark/proxies/traefik/traefik.yaml docker-compose \
+        --file /tmp/sla-gateway-benchmark/proxies/traefik/docker-compose-traefik.yaml up \
         --detach ;
 else
     CFG_PATH=/tmp/proxy-configuration-file docker-compose \
-        --file /tmp/autocloned-sla-gateway-benchmark/proxies/$PROXY/docker-compose-$PROXY.yaml up \
+        --file /tmp/sla-gateway-benchmark/proxies/$PROXY/docker-compose-$PROXY.yaml up \
         --detach ;
 fi # > /dev/null 2>&1 ;
 
--- tests/test_cleanup.sh
+++ tests/test_cleanup.sh
@@ -1,7 +1,7 @@
 echo "# Tearing down test bed";
 
 for proxy in envoy haproxy nginx traefik; do
-    docker-compose --file /tmp/autocloned-sla-gateway-benchmark/proxies/$proxy/docker-compose-$proxy.yaml down > /dev/null 2>&1
+    docker-compose --file /tmp/sla-gateway-benchmark/proxies/$proxy/docker-compose-$proxy.yaml down > /dev/null 2>&1
 done
 
-rm -fr /tmp/generatedSLAs /tmp/autocloned-sla-gateway-benchmark
+rm -fr /tmp/generatedSLAs /tmp/sla-gateway-benchmark
